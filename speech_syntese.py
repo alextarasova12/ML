08e10e54144ba807ce7bb5a3f20871948ad4ce30
Ron <80053176+pohbl4@users.noreply.github.com>
2024-06-03 21:01:49 +0500

    Update speech_syntese.py

--- speech_syntese.py
+++ speech_syntese.py
@@ -1,15 +1,18 @@
 import re
 from transformers import AutoTokenizer, AutoModelWithLMHead
 
-tokenizer = AutoTokenizer.from_pretrained("tinkoff-ai/ruDialoGPT-medium")
-model = AutoModelWithLMHead.from_pretrained("tinkoff-ai/ruDialoGPT-medium")
-
+try:
+    tokenizer = AutoTokenizer.from_pretrained("tinkoff-ai/ruDialoGPT-medium")
+    model = AutoModelWithLMHead.from_pretrained("tinkoff-ai/ruDialoGPT-medium")
+except Exception as e:
+    print(f"Ошибка при загрузке модели или токенизатора: {e}")
+    exit(1)
 
 def generate_answer(input_question):
-    prepared_input = "@@ПЕРВЫЙ@@ " + input_question + " @@ВТОРОЙ@@ "
+    prepared_input = f"{tokenizer.bos_token} {input_question} {tokenizer.eos_token}"
     inputs = tokenizer(prepared_input, return_tensors="pt")
     generated_token_ids = model.generate(
-        **inputs,
+         *  * inputs,
         top_k=10,
         top_p=0.95,
         num_beams=3,
@@ -19,7 +22,7 @@ def generate_answer(input_question):
         temperature=1.2,
         repetition_penalty=1.2,
         length_penalty=1.0,
-        eos_token_id=50257,
+        eos_token_id=tokenizer.eos_token_id,
         max_new_tokens=40
     )
     context_with_response = [
@@ -27,7 +30,6 @@ def generate_answer(input_question):
         for sample_token_ids in generated_token_ids
     ]
     answer = re.sub(
-        "@@ПЕРВЫЙ@@.*"
-        + "?@@ВТОРОЙ@@", "", context_with_response[0], flags=re.DOTALL
+        f"^{tokenizer.bos_token}. * ?{tokenizer.eos_token}", "", context_with_response[0], flags=re.DOTALL
     )
-    return answer.replace("@@ПЕРВЫЙ@@", "").replace("@@ВТОРОЙ@@", "")
+    return answer.replace(tokenizer.bos_token, "").replace(tokenizer.eos_token, "")
