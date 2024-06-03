9b300ec1c74f5a6b0356b6c7485ff800fe88cfe0
Ron <80053176+pohbl4@users.noreply.github.com>
2024-06-03 20:55:56 +0500

    Update web_form.py

--- web_form.py
+++ web_form.py
@@ -4,21 +4,21 @@ import speech_syntese as ss
 
 def main():
     st.title("Добро пожаловать в наш чат. Спросите меня что-нибудь")
-    prompt = st.chat_input(placeholder="Начните разговор")
+    prompt = st.text_input("Введите ваш вопрос здесь:")
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
+        with st.expander(f"{message['role']}"):
             st.markdown(message["content"])
 
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
+        with st.expander("user"):
             st.markdown(prompt)
 
-        with st.chat_message("assistant"):
+        with st.expander("assistant"):
             message_placeholder = st.empty()
             full_response = ss.generate_answer(prompt)
             message_placeholder.markdown(full_response)
