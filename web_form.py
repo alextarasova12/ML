5233e5fcf285e2c76a9ba2bcf11ddc1e32d51533
Ron <80053176+pohbl4@users.noreply.github.com>
2024-06-03 21:43:16 +0500

    Update web_form.py

--- web_form.py
+++ web_form.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import speech_syntese as ss
 
-
 def main():
     st.title("Добро пожаловать в наш чат. Спросите меня что-нибудь")
     prompt = st.text_input("Введите ваш вопрос здесь:")
@@ -10,22 +9,21 @@ def main():
         st.session_state.messages = []
 
     for message in st.session_state.messages:
-        with st.expander(f"{message['role']}"):
-            st.markdown(message["content"])
+        st.markdown(f"### {message['role']}")
+        st.write(message["content"])
 
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.expander("user"):
-            st.markdown(prompt)
+        st.markdown(f"### user")
+        st.write(prompt)
 
-        with st.expander("assistant"):
-            message_placeholder = st.empty()
-            full_response = ss.generate_answer(prompt)
-            message_placeholder.markdown(full_response)
-        st.session_state.messages.append(
-            {"role": "assistant", "content": full_response}
-        )
-
+        response = ss.generate_answer(prompt)
+        if response is not None:
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.markdown(f"### assistant")
+            st.write(response)
+        else:
+            st.warning("Извините, в данный момент у меня возникли проблемы с генерацией ответа. Могу ли я помочь вам с чем-то еще?")
 
 if __name__ == "__main__":
     main()
