bf8cca73ce5c808d53b90beb937e62a6ad647243
Ron <80053176+pohbl4@users.noreply.github.com>
2024-06-03 20:45:38 +0500

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,15 +1,14 @@
 import unittest
+import speech_syntese as ss
 from unittest.mock import patch
 
 class TestSpeechSyntese(unittest.TestCase):
     @patch('speech_syntese.some_external_api_call')
     def test_generate_answer(self, mock_api_call):
-        mock_api_call.return_value = "Ожидаемый ответ"
-        
         answer = ss.generate_answer("Привет")
-        self.assertEqual(answer, "Ожидаемый ответ")
+        self.assertNotEmpty(answer) 
         
-        mock_api_call.assert_called_with("Привет")
+        mock_api_call.assert_called_with("Привет") 
 
 if __name__ == '__main__':
     unittest.main()
